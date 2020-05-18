38d79cc9fa970c6a5591e95620e74cb5f2bf4033
Michal Gornik <michalgornik93@gmail.com>
2020-05-18 10:23:27 +0200

    AddMigrationsForWorshopDataContext

---
+++ MaintenanceDashboard.Data/Migrations/202005180821271_AddDataModelWorkshopDbContext.cs
@@ -0,0 +1,49 @@
+﻿namespace MaintenanceDashboard.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDataModelWorkshopDbContext : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false),
+                        LastName = c.String(nullable: false),
+                        UidCode = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Paddles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.RegisterTools",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ToolName = c.String(nullable: false),
+                        UidCode = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.RegisterTools");
+            DropTable("dbo.Paddles");
+            DropTable("dbo.Employees");
+        }
+    }
+}
