9a02948738473f855c7a78d421f07e967e6cadda
Michal Gornik <michalgornik93@gmail.com>
2020-05-26 07:59:21 +0200

    AddedEmployeeNamePropertyToViewModel

--- Maintenance dashboard.Client/ViewModels/RegisterToolModel.cs
+++ Maintenance dashboard.Client/ViewModels/RegisterToolModel.cs
@@ -1,9 +1,22 @@
 ﻿using MaintenanceDashbord.Library;
+using System.ComponentModel.DataAnnotations;
 
 namespace MaintenanceDashboard.ViewModels
 {
     public class RegisterToolModel:ViewModel
     {
+        private string employeeName;
 
+        [Required]
+        [StringLength(32, MinimumLength =2)]
+        public string EmployeeName
+        {
+            get { return employeeName; }
+            set
+            {
+                employeeName = value;
+                NotifyPropertyChanged();
+            }
+        }
 	}
 }
